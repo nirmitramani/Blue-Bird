0dc0570822469144b6f5a4a1569a26951a466ec9
Nirmit Ramani <143725377+nirmitramani@users.noreply.github.com>
2024-05-08 22:12:24 +0530

    databse connection

--- server/config/DatabseConnection.js
+++ server/config/DatabseConnection.js
@@ -1,6 +1,14 @@
-const mongoose = require('mongoose');
-const constant = require('./Constant');
+const mongoose = require("mongoose");
+const constant = require("./Constant");
 
-mongoose.connect(process.env.DB_URL)
-  .then(() => console.log(constant.MSG_FOR_CONNECTION_SUCCESSFUL))
-  .catch(error => console.error(constant.MSG_FOR_CONNECTION_ERROR, error));
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log(constant.MSG_FOR_CONNECTION_SUCCESSFUL);
+  })
+  .catch((error) => {
+    console.error(constant.MSG_FOR_CONNECTION_ERROR, error);
+  });
