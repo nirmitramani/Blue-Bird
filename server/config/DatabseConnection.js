921228a483ce849f9e7c3e37d61ea3ccbb40d562
Nirmit Ramani <143725377+nirmitramani@users.noreply.github.com>
2024-05-09 18:50:52 +0530

    databse connection 2

--- server/config/DatabseConnection.js
+++ server/config/DatabseConnection.js
@@ -3,8 +3,8 @@ const constant = require("./Constant");
 
 // Establish connection to MongoDB
 mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
   })
   .then(() => {
     console.log(constant.MSG_FOR_CONNECTION_SUCCESSFUL);
